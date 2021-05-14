bee9818edc54971f8f35889892469e1e66b17fb5
Sebastian Andres <sebastianandres@MacBook-Air-de-Sebastian.local>
2021-05-13 22:03:31 -0400

    category_id:add

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@ class Category < ApplicationRecord
     has_many :bookmark_categories
     has_many :bookmarks, through: :bookmark_categories, dependent: :destroy
 
+    belongs_to :parent_category, class_name: 'Category', foreign_key: :category_id, optional: true
+    has_many :child_categories, class_name: 'Category', foreign_key: :category_id, dependent: :delete_all
+    
     def to_s
         self.name
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_213404) do
+ActiveRecord::Schema.define(version: 2021_05_14_013627) do
 
   create_table "bookmark_categories", force: :cascade do |t|
     t.integer "bookmark_id", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_213404) do
     t.boolean "public"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
   create_table "kinds", force: :cascade do |t|
